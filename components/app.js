c21d9ca35eb85efa318b08bb2584ae56cb9619c0
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-03-22 10:40:43 -0700

    added app template

---
+++ components/app.js
@@ -0,0 +1,9 @@
+angular.module('themechanger').component('app', {
+    templateUrl:'templates/app.html',
+    controller: function(){
+        this.allowEdit = true;
+        this.fontColor = 'black';
+    },
+    controllerAs: 'myCtrl'
+})
+
